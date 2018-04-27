fbc8b9e453ba7bac6f5813a4436ce94946b18704
Lynn Hu <lynn.yang.hu@gmail.com>
2018-04-26 20:33:30 -0700

    Updating 'var' to 'let'

--- js/lichtenstein.js
+++ js/lichtenstein.js
@@ -1,8 +1,8 @@
-var h = 800;
-var w = window.innerWidth / 2;
-var spacer = 8;
-var dotSize = 5;
-var rowNum;
+let h = 800;
+let w = window.innerWidth / 2;
+let spacer = 8;
+let dotSize = 5;
+let rowNum;
 
 function setup() {
   var bendayBg = createCanvas(w, h);
@@ -11,8 +11,8 @@ function setup() {
   rowNum = 0;
   fill(0, 0, 0);
   noStroke();
-  for (var y = 0; y < h+1; y += spacer) {
-    for (var x = 0; x < w+1; x += spacer) {
+  for (let y = 0; y < h+1; y += spacer) {
+    for (let x = 0; x < w+1; x += spacer) {
       if ( rowNum % 2 == 0) {
         ellipse(x + spacer/2, y + spacer/2, dotSize, dotSize);
       } else {
@@ -22,85 +22,3 @@ function setup() {
     rowNum ++;
   }
 }
-
-
-// // Height of canvas is the same as heigh of div
-// var h = 800;
-// var w = window.innerWidth / 2;
-// var spacer = 8;
-// var dotSize = 5;
-// var bendays = [];
-//
-// function setup(){
-//   var bg = createCanvas(w, h);
-//   bg.parent('benday');
-//   noStroke();
-//   initializeArray();
-//   drawBackground();
-//   placeBenday();
-// }
-//
-// function placeBenday() {
-//   for(var i = 0; i < bendays.length; i++){
-//     bendays[i].draw();
-//   }
-// }
-//
-// function mouseDragged() {
-//   for (var i = bendays.length - 1; i >= 0; i --) {
-//     if(bendays[i].touching(mouseX, mouseY) && mouseIsPressed) {
-//       bendays.splice(i, 1);
-//     }
-//   }
-//   drawBackground();
-//   placeBenday();
-// }
-//
-// function drawBackground(){
-//   background(247, 246, 227);
-//   var rowNum = 0;
-//   for (var y = 0; y < h; y += spacer) {
-//     for (var x = 0; x < w; x += spacer) {
-//       if (rowNum % 2 == 0) {
-//         fill(171, 28, 42);
-//         ellipse(x + spacer / 2, y + spacer / 2, dotSize);
-//       } else {
-//         fill(171, 28, 42);
-//         ellipse(x, y + spacer / 2, dotSize);
-//       }
-//     }
-//     rowNum ++;
-//   }
-// }
-//
-// function initializeArray() {
-//   var rowNum = 0;
-//   for (var y = 0; y < h; y+= spacer){
-//     for(var x = 0; x < w; x += spacer) {
-//       if (rowNum % 2 == 0) {
-//         bendays.push(new Benday(x + spacer / 2, y + spacer / 2));
-//       } else {
-//         bendays.push(new Benday(x, y + spacer / 2));
-//       }
-//     }
-//   }
-// }
-//
-// class Benday {
-//   constructor(x, y) {
-//     this.x = x;
-//     this.y = y;
-//     this.mouseSize = 8;
-//     this.size = dotSize + 2;
-//     this.color = color(247, 246, 227, 255);
-//   }
-//
-//   draw() {
-//     fill(this.color);
-//     ellipse(this.x, this.y, this.size);
-//   }
-//
-//   touching(x, y){
-//     return dist(this.x, this.y, x, y) < (this.size/2 + this.mouseSize / 2);
-//   }
-// }
